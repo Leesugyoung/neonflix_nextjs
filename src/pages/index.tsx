0291bced4dc8fd0a0ee04450adf98d921bb227ce
sugyoung lee <tnrud4685@naver.com>
2023-01-31 02:26:35 +0900

    test4

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,22 @@
+interface IMovieProps {
+  id: number;
+  backdrop_path: string;
+  original_title: string;
+  overview: string;
+  poster_path: string;
+  title: string;
+  vote_average: number;
+  genre_ids: [number];
+  release_date: string;
+}
+
 export default function Home() {
   return (
     <>
+      <h1>hiiiiiiiiiii</h1>
+      <h1>hiiiiiiiiiii</h1>
+      <h1>hiiiiiiiiiii</h1>
+      <h1>hiiiiiiiiiii</h1>
       <h1>hiiiiiiiiiii</h1>
     </>
   );
