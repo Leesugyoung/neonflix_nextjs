7acfb1f595d4d404fa744a4494f4b663fb36bec6
sugyoung lee <tnrud4685@naver.com>
2023-01-31 02:31:52 +0900

    test5

--- next.config.js
+++ next.config.js
@@ -1,6 +1,45 @@
 /** @type {import('next').NextConfig} */
+const API_KEY = process.env.API_KEY;
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+      },
+    ],
+  },
+  experimental: {
+    appDir: true,
+  },
+  async rewrites() {
+    return [
+      {
+        // movie(메인) 페이지
+        // Rewrites를 사용하면 들어오는 request 경로를 다른 destination 경로에 매핑할 수 있다.
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+        // Rewrites은 URL 프록시 역할을 하고 destination 경로를 mask하여 사용자가 사이트에서 위치를 변경하지 않은 것처럼 보이게 합니다. 반대로 redirects은 새 페이지로 reroute되고 URL 변경 사항을 표시합니다.
+      },
+      {
+        // movie detail 페이지
+        source: "/api/movies/:id",
+        destination: `https://api.themoviedb.org/3/movie/:id?api_key=${API_KEY}`,
+      },
+      {
+        // series 페이지
+        source: "/api/tv",
+        destination: `https://api.themoviedb.org/3/tv/popular?api_key=${API_KEY}`,
+      },
+      {
+        // series detail 페이지
+        source: "/api/tv/:id",
+        destination: `https://api.themoviedb.org/3/tv/:id?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,10 @@
+import Seo from "@/components/Seo";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import main from "../styles/main.module.css";
+
 interface IMovieProps {
   id: number;
   backdrop_path: string;
@@ -10,14 +17,88 @@ interface IMovieProps {
   release_date: string;
 }
 
-export default function Home() {
+export default function Home({
+  results,
+}: InferGetServerSidePropsType<GetServerSideProps>) {
+  const router = useRouter();
+  const onClick = (id: number, title: string) => {
+    router.push({
+      pathname: `/movies/${title}/${id}`,
+    });
+  };
   return (
     <>
-      <h1>hiiiiiiiiiii</h1>
-      <h1>hiiiiiiiiiii</h1>
-      <h1>hiiiiiiiiiii</h1>
-      <h1>hiiiiiiiiiii</h1>
-      <h1>hiiiiiiiiiii</h1>
+      <div className={main.container}>
+        <Link
+          href={`/movies/${results[0].title}/${results[0].id}`}
+          legacyBehavior
+        >
+          <Image
+            src={`https://image.tmdb.org/t/p/w500${results[0].backdrop_path}`}
+            alt="results[0].backdrop_path"
+            width={520}
+            height={310}
+            priority
+            quality={100}
+          />
+        </Link>
+        <div className={main.gradient} />
+        <div className={main.banner_Title}>
+          <a onClick={() => onClick(results[0].id, results[0].original_title)}>
+            {results[0].title}
+          </a>
+        </div>
+        <div className={main.banner_minititle}>
+          <span>{results[0].release_date.substring(0, 4)}</span>
+          <span>
+            ⭐ {results[0].vote_average ? results[0].vote_average : "0"}
+          </span>
+        </div>
+        <div className={main.Trending}>Trending Now</div>
+        <div className={main.gird_container}>
+          <Seo title="Home" />
+          {results?.map((movie: IMovieProps, index: number) => {
+            if (index !== 0) {
+              return (
+                <Link
+                  href={`/movies/${movie.original_title}/${movie.id}`}
+                  key={movie.id}
+                  legacyBehavior
+                >
+                  <a onClick={() => onClick(movie.id, movie.original_title)}>
+                    <div className={main.grid_Itemlist}>
+                      <img
+                        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                      />
+                      <div className={main.Info}>
+                        <div className={main.Info_title}>
+                          {movie.title || movie.original_title}
+                          <h4 className={main.Info_average}>
+                            ⭐ {movie.vote_average ? movie.vote_average : "0"}
+                          </h4>
+                        </div>
+                      </div>
+                    </div>
+                  </a>
+                </Link>
+              );
+            }
+          })}
+        </div>
+      </div>
     </>
   );
+}
+
+export async function getServerSideProps({}: GetServerSideProps) {
+  // 이 곳에 적힌 코드들은 서버에서만 실행됨!
+  const { results } = await (
+    await fetch(`http://localhost:3000/api/movies`)
+  ).json();
+  return {
+    props: {
+      // -> pageProps
+      results,
+    },
+  };
 }
